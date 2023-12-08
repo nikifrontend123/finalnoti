512a4994b6da99277a9ce6a9f322d7bb13156013
Abhishekfpaipl <abhishekfpaipl@gmail.com>
2023-12-08 15:23:57 +0530

    checking local storage data

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -15,31 +15,16 @@ register(`${process.env.BASE_URL}sw.js`, {
     if ('Notification' in window) {
       Notification.requestPermission().then(permission => {
         if (permission === 'granted') {
-          registration.pushManager.getSubscription().then(() => {
-            // if (existingSubscription) {
-            //   console.log('User is already subscribed to push notifications:', existingSubscription);
-            //   localStorage.setItem('p256dhKey', existingSubscription.keys.p256dh);
-            //   localStorage.setItem('endpoint', existingSubscription.endpoint);
-            //   localStorage.setItem('authKey', existingSubscription.keys.auth);
-            // } else {
-              // User is not subscribed; register for push notifications
-              registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: 'BHaGoupz6SaaiUM6EOTtsVSVjAklaOV3Y4lmexYmEV7XwDDiA4LkPLfqmvpaF4FcyyHEZ2LvLQUp9sHpuW0K96s'
-              }).then(function (newSubscription) {
-                console.log('Push subscription successful:', newSubscription);
-                localStorage.setItem('p256dhKey', newSubscription.keys.p256dh);
-                localStorage.setItem('endpoint', newSubscription.endpoint);
-                localStorage.setItem('authKey', newSubscription.keys.auth);
-              }).catch(error => {
-                console.error('Error subscribing to push notifications:', error);
-              });
-            // }
-            console.log('Permission Granted');
-            console.log(localStorage.getItem('p256dhKey'));
-            console.log(localStorage.getItem('endpoint'));
-            console.log(localStorage.getItem('authKey'));
-
+          registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: 'BHaGoupz6SaaiUM6EOTtsVSVjAklaOV3Y4lmexYmEV7XwDDiA4LkPLfqmvpaF4FcyyHEZ2LvLQUp9sHpuW0K96s'
+          }).then(function (newSubscription) {
+            console.log('Push subscription successful:', newSubscription);
+            localStorage.setItem('p256dhKey', newSubscription.keys.p256dh);
+            localStorage.setItem('endpoint', newSubscription.endpoint);
+            localStorage.setItem('authKey', newSubscription.keys.auth);
+          }).catch(error => {
+            console.error('Error subscribing to push notifications:', error);
           });
         }
       });
